2054dc39ea1522d9c8b61977d01d39a6c5dbf6a4
unknown <haymeemotoh22@gmail.com>
2023-04-16 23:28:03 +0800

    Finalize Project

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
-import QRCodeGenerator from "@/components/QRCodeGenerator"
-import {useState, useEffect} from 'react'
+import QRCodeGenerator from "@/components/QRCodeGenerator";
 import useDebounce from '@/hooks/useDebounce';
 import Head from "next/head";
+import { useState } from 'react';
 
 export default function Home() {
   const [textToPass, setTextToPass] = useState<string>('')
@@ -11,18 +11,17 @@ export default function Home() {
     setTextToPass(e.currentTarget.value);
   }
 
-  const download = (e: React.MouseEvent<HTMLButtonElement>): void => {
+  const downloadQRCode = (e: React.MouseEvent<HTMLButtonElement>): void => {
     e.preventDefault();
 
     const qrCodeURL = document.getElementById('generatedQRCode') as HTMLCanvasElement;
     const pngUrl = qrCodeURL
     .toDataURL("image/png")
     .replace("image/png", "image/octet-stream");
-    console.log(qrCodeURL)
 
-    let downloadLink = document.createElement("a");
+    const downloadLink = document.createElement("a");
     downloadLink.href = pngUrl;
-    downloadLink.download = "123456.png";
+    downloadLink.download = "QRCode.png";
     document.body.appendChild(downloadLink);
     downloadLink.click();
     document.body.removeChild(downloadLink);
@@ -32,8 +31,13 @@ export default function Home() {
     <>
       <Head>
         <title>QR Code Generator</title>
+        <meta name="description" content="Web App for Transforming any form of Text into a QR Code that can download" />
+        <meta http-equiv="Content-Type" content="text/html;charset=UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="keywords" content="QR Code, Text To QR, Generate QR Code"/>
+        <meta name="robots" content="follow"/>
       </Head>
-      <main className="flex justify-center items-center h-screen m-auto">
+      <main className="flex justify-center items-center h-screen m-auto min-w-fit">
         <div className="card text-white rounded-3xl w-1/5 drop-shadow-2xl min-w-fit">
           <QRCodeGenerator textToQR={debounceValue}/>
           <h3 className="cardBoldText font-bold text-center text-lg pb-5 px-3">
@@ -43,9 +47,14 @@ export default function Home() {
           <p className="cardNormalText font-normal text-center m-3">
             Scan the QR code to visit<br/>the link or view the text.
           </p>
+          <button onClick={downloadQRCode} className="block m-3 p-2 border-2 mx-auto rounded-lg w-60">Download</button>
         </div>
-        <button onClick={download}>Download</button>
       </main>
+      <footer>
+        <p className="w-full text-center -mt-10">
+          Designed by <a href="http://frontendmentor.com/">FRONTENDMENTOR</a>, Coded by <a href="https://github.com/JamesRomero2">JAMES ROMERO</a>
+        </p>
+      </footer>
     </>
   )
 }
